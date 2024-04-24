6b43b4f54b7e8759ceff2dd6047ba8db59371f1d
Evgeniy <zhilkevich212@gmail.com>
2024-04-24 17:03:19 +0300

    finish adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -2,18 +2,19 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const navBtn = document.querySelector('.mobile-btn'),
     menuItem = document.querySelectorAll('.header__menu-item'),
-    nav = document.querySelector('.header__menu');
+    nav = document.querySelector('.header__wrapper');
 
     navBtn.addEventListener('click', () => {
         navBtn.classList.toggle('mobile-btn_open');
-        nav.classList.toggle('header__menu_open');
+        nav.classList.toggle('header__wrapper_open');
         document.body.classList.toggle('no-scroll');
     });
 
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
             navBtn.classList.toggle('mobile-btn_open');
-            nav.classList.toggle('header__menu_open');
+            nav.classList.toggle('header__wrapper_open');
+            document.body.classList.toggle('no-scroll');
         })
     });
 
